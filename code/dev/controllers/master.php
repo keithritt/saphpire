b05a9f3caa569bbfe2daca367e962fac2e23de05
Keith Ritt <contact@keithritt.net>
2019-11-07 00:24:53 -0500

    Update master.php

--- code/dev/controllers/master.php
+++ code/dev/controllers/master.php
@@ -10,11 +10,7 @@ class Master
 
   public static function launch($sMethod, $aArgs)
   {
-    //line();
-    //pr('Master::launch('.$sMethod.')');
-    //expose($aArgs);
     $oController = new Controller();
-    //line();
     $aArgs = array_values($aArgs);
     switch(count($aArgs))
     {
@@ -40,56 +36,20 @@ class Master
         $oController->$sMethod($aArgs[0], $aArgs[1], $aArgs[2], $aArgs[3], $aArgs[4]);
         Log::error('Master::launch() with over 5 arguments');
         break;
-
     }
-
   }
 
 	function __construct()
 	{
-		//pr('Master->__construct()');
-    //line();
-
-		//Session::init();
-		//Request::record_page_view();
-    //line();
-		//define('PAGE_VIEW_ID', $this->iPageViewId);
-
 		register_shutdown_function(array('Request', 'update_page_view'));
-		//expose($this->iPageViewId);
-    //line();
-
-
-		//$this->bRequireLogin = @Util::coalesce($this->bRequireLogin, true);
-    //line();
-
-
 		$this->oDb = new Db('master');
-    //line();
-
-		//$this->data = array();
-
-    //$this->aCss = array();
-    //$this->aJs = array();
     $this->aSubTemplates = @Util::coalesce($this->aSubTemplates, array());
 
-    //line();
-
-
-
     // TODO -  not sure if im 100% happy with this being in the constructor - cant always do perm checks this early
-    //line();
     $this->check_login();
-    //line();
-
-
-		//expose($_POST);
 		if(isset($_POST['bAllowViewPw']))
 			Auth::check_allow_view_pass($_POST['bAllowViewPw']);
-		//expose($_COOKIE);
 		$this->bAllowView = @Util::coalesce($_POST['bAllowView'], $_SESSION['bAllowView'], $_COOKIE['bAllowView'], (ENV == 'prod'));
-
-    //line();
 
     $oMobileDetect = new Mobile_Detect;
     if($oMobileDetect->isMobile())
@@ -99,12 +59,6 @@ class Master
     else
       $this->sScreenSize = 'lg'; //@TODO - determine way to tell md from lg - use session data
 
-    //line();
-
-		//stop();
-
-
-		//setlocale(LC_MONETARY, 'en_US');
 	}
 
   public function init()
@@ -124,51 +78,28 @@ class Master
 
   private function check_login()
   {
-    //pr('Master::check_login');
-    //expose(Perm::$bRequireLogin);
     if(Perm::$bRequireLogin)
     {
       $this->aLogin = Session::fetch_login();
-      //expose($this->aLogin);
-      //stop();
       if(!$this->aLogin)
       {
-        //Perm::ignore();
         //@TODO - set some flashdata to display a sesson timeout issue
         Request::redirect('/login/leave');
       }
     }
-    //else
-    //  Perm::$bChecked = true;
   }
 
 	public function __destruct()
 	{
-		//pr('__destruct()');
 		if(class_exists('Permission') && !Permission::$bChecked)
 			Log::error('no permission check on: '.get_full_url());
 	}
 
 	protected function get_template($sTemplate, $sTemplateId = null)
 	{
-		//pr('get_template('.$sTemplate.')');
-    //stop();
-
-		//$sDefaultCss
-
 		$sRet = '';
 		$sRet.= "\n<!--BEGIN TEMPLATE $sTemplate -->\n";
 
-		//if(!isset($this->sLayout))
-		//		Log::error('Missing Layout');
-
-		//pr($sTemplate);
-
-		//expose(file_exists(PUBLIC_HTML_PATH.'/'.ENV.'/application/templates/'.$sTemplate));
-
-		//stop();
-
-		//expose($sTemplate);
 
     $sFile = CODE_PATH.'/'.$sTemplate;
 
@@ -188,12 +119,8 @@ class Master
 
   public static function add_js($sFile, $sRegion = 'body_end', $bAddBuildId = true)
   {
-    //pr($sFile);
     if(Request::$bDebugMode && (!file_exists(ltrim($sFile, '/')) || strpos($sFile, '/') != 0) )
       Log::Error('add_js() - invalid file: '.$sFile);
-
-  	//pr('add_js('.$sFile.')');
-    //expose_backtrace();
   	if($bAddBuildId)
     {
   		$sSuffix = '?v='.Request::$iBuildId;
@@ -203,18 +130,13 @@ class Master
   	else
   		$sSuffix = '';
 
-  	//$oThis = &get_instance();
   	self::$aJs[$sRegion][] = $sFile.$sSuffix;
   }
 
   public static function add_css($sFile, $sRegion = 'head_start', $bAddBuildId = true)
   {
-    //pr($sFile);
     if(Request::$bDebugMode && (!file_exists(ltrim($sFile, '/')) || strpos($sFile, '/') != 0) )
       Log::Error('add_css() - invalid file: '.$sFile);
-
-
-
   	if($bAddBuildId)
     {
   		$sSuffix = '?v='.Request::$iBuildId;
@@ -243,8 +165,6 @@ class Master
 
   public static function add_footer_js($sJs)
   {
-    //if(!isset($this->sFooterJs))
-    //    $this->sFooterJs = '';
     self::$sFooterJs.= "$sJs ; \n";
   }
 

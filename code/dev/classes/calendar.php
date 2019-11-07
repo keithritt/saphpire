bef4a3ae2d440bd89c1df5684eb5c4c44419be7c
Keith Ritt <contact@keithritt.net>
2019-11-06 23:45:55 -0500

    Update calendar.php

--- code/dev/classes/calendar.php
+++ code/dev/classes/calendar.php
@@ -1,13 +1,9 @@
 <?
 
-// currenty used by:
-// barmend.com/admin/calendar
-
 class Calendar
 {
   public function __construct($sId, /*$sTheme = 'bootstrap_big'*/ $iMonth = null, $iYear = null)
   {
-    //pr('calendar->__construct('.$sId.')');
     $this->sId = $sId; //'calendar'; // can override with set_id()
     $this->iMonth = Util::coalesce($iMonth, date('n'));
     $this->iYear = Util::coalesce($iYear, date('Y'));
@@ -16,24 +12,11 @@ class Calendar
     $this->iMonthStartDay = date('w', $this->iMonthStart) + 1;
     $this->iDaysInMonth = date('t', $this->iMonthStart);
     $this->sRefreshUrl = '/calendar/get_data';
-
-    //expose($this->iMonth);
-    //expose($this->iYear);
-    //expose($this->sMonthStart);
-    //expose($this->iMonthStartDay);
-    //expose($this->iMonthStart);
-    //expose(date('n/j/y', $this->iMonthStart));
-
     $this->sTheme = 'bootstrap_big'; //$sTheme;
 
     $this->set_configs();
   }
-
-  //public function set_id($sId)
-  //{
-  //  $this->sId = $sId;
-  //}
-
+  
   // with the hard coding of the js var name - this might not be the best place for this logic
   public function get_js()
   {
@@ -55,14 +38,9 @@ class Calendar
   public function get_html()
   {
     $this->custom_content();
-    //return;
     $aHtml = array();
 
     $aHtml[] = '<div id="calendar_container" class="hidden-xs">';
-    //$aHtml[] = 'calendar';
-
-
-
     $aHtml[] = $this->get_header();
     $aHtml[] = $this->get_calendar();
     $aHtml[] = $this->get_footer();
@@ -76,8 +54,6 @@ class Calendar
   // @TODO calendar doesnt render well on small screens
   public function get_upcoming_html()
   {
-    //print '<br>get_upcoming_html()';
-    //stop();
     $aHtml = array();
     if(count($this->aUpcomingEvents))
     {
@@ -97,14 +73,12 @@ class Calendar
       $aHtml[] = '</div">';
     }
     return implode($aHtml, "\n");
-
   }
 
 
   private function get_header()
   {
     $aHtml = array();
-    //$aHtml[] = 'header';
 
     $aHtml[] = '<div class="center bold" style="'.$this->sHeaderStyle.'">';
     $aHtml[] = '<span class="clickable glyphicon glyphicon-menu-left" onclick="oNs.calendars.'.$this->sId.'.prev_month();"></span>';
@@ -127,12 +101,8 @@ class Calendar
   private function get_footer()
   {
     $aHtml = array();
-    //$aHtml[] = 'header';
-
     $aHtml[] = '<div class="center bold" style="'.$this->sFooterStyle.'">';
-
     $aHtml[] = '<span class="clickable glyphicon glyphicon-refresh" onclick="oNs.calendars.'.$this->sId.'.refresh();" ></span>';
-
     $aHtml[] = '</div>';
     $aHtml[] = '<br/>';
 
@@ -166,22 +136,17 @@ class Calendar
       $aHtml[] = '<tr>';
       for($iDayWalk = 1; $iDayWalk <= 7; $iDayWalk++)
       {
-        //pr($iDayWalk);
-        //pr($this->iMonthStart);
         $aHtml[] =  '<td style="'.$this->sTdStyle.'" class="'.$this->sTdClass.'">';
         if($iWeekWalk == 1 && $iDayWalk  < $this->iMonthStartDay)
         {
-          //line();
           // do nothing
         }
         elseif($iDateWalk > $this->iDaysInMonth)
         {
-          //line();
           // do nothing
         }
         else
         {
-          //line();
           $aHtml[] = '<div style="text-align:right;">';
           $aHtml[] = $iDateWalk;
           $aHtml[] = '</div>';
@@ -207,13 +172,10 @@ class Calendar
       return implode($aHtml, "\n");
   }
 
-
-
   private function set_configs()
   {
     $this->iCellWidth = 100;
     $this->iCalendarWidth = $this->iCellWidth * 7;
-    //$this->sThStyle = 'width: '.$this->iCellWidth.'px;';
     $this->sThStyle = 'width: 12.5%;';
     $this->sThClass = 'center bold';
 
@@ -233,33 +195,25 @@ class Calendar
     background: white;';
     $this->sTdClass = 'center';
 
-    switch($this->sTheme)
-    {
-
-    }
   }
 
   public function add_one_time_content($sDate, $sContent)
   {
-    //pr('add_yearly_content('.$sDate.', '.$sContent.')');
     $this->aContent['one_time'][$sDate] = $sContent;
   }
 
   public function add_yearly_content($sDate, $sContent)
   {
-    //pr('add_yearly_content('.$sDate.', '.$sContent.')');
     $this->aContent['yearly'][$sDate] = $sContent;
   }
 
   public function add_monthly_content($sDate, $sContent)
   {
-    //pr('add_monthly_content('.$sDate.', '.$sContent.')');
     $this->aContent['monthly'][$sDate] = $sContent;
   }
 
   public function add_weekly_content($vDay, $sContent)
   {
-    //pr('add_weekly_content('.$vDay.', '.$sContent.')');
     switch($vDay)
     {
       case TYPE_DAY_SUN:
@@ -291,156 +245,39 @@ class Calendar
     $this->aContent['weekly'][$iDayId] = $sContent;
   }
 
-  // @TODO - move out of default calendar class
-  private function custom_content()
-  {
-    switch($this->sId)
-    {
-      case 'barmend_admin':
-        //global $oThis;
-
-        //expose($oThis);
-        $sSql = "
-        SELECT
-          id,
-          title,
-          interval_id,
-          interval_desc
-        FROM
-          event_templates
-        WHERE
-          bar_id = ".(int)Controller::_get('bar_id');
-
-        //expose($sSql);
-
-        $aRows = Db::$oBarmend->select_rows($sSql);
-
-        //expose($aRows);
-
-        foreach($aRows as $aRow)
-        {
-          //expose($aRow['title']);
-
-
-
-          if($aRow['title'] == '')
-            $aRow['title'] = 'untitled';
-          switch($aRow['interval_id'])
-          {
-            case TYPE_INTERVAL_ONE_TIME:
-              $this->add_one_time_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="edit_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_WEEKLY:
-              //$oModel->interval_desc = $_POST['weekday_id'];
-              $this->add_weekly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="edit_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_MONTHLY:
-              $this->add_monthly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="edit_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_YEARLY:
-              $this->add_yearly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="edit_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-          }
-        }
-        break;
-      case 'bar_calendar':
-        //global $oThis;
-
-        $sSql = "
-        SELECT
-          id,
-          title,
-          interval_id,
-          interval_desc
-        FROM
-          event_templates
-        WHERE
-          bar_id = ".(int)Controller::_get('bar_id');
-
-        //expose($sSql);
-
-        $aRows = Db::$oBarmend->select_rows($sSql);
-
-        //expose($aRows);
-
-        $this->add_upcoming_content($aRows);
-
-        foreach($aRows as $aRow)
-        {
-
-
-          //expose($aRow['title']);
-          //$aRow['title'] = coalesce($aRow['title'], 'untitled');
-          switch($aRow['interval_id'])
-          {
-            case TYPE_INTERVAL_ONE_TIME:
-              $this->add_one_time_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="view_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_WEEKLY:
-              //$oModel->interval_desc = $_POST['weekday_id'];
-              $this->add_weekly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="view_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_MONTHLY:
-              $this->add_monthly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="view_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-            case TYPE_INTERVAL_YEARLY:
-              $this->add_yearly_content($aRow['interval_desc'], '<div class="clickable underlined" onclick="view_event('.$aRow['id'].')">'.$aRow['title'].'</div>');
-              break;
-          }
-        }
-        break;
-    }
-
-  }
-
   private function add_upcoming_content($aEventData)
   {
-    //expose('add_upcoming_content()');
-
     $aCalcData = array();
 
     $iStart = strtotime('today');
     $iEnd = strtotime('+ 30 days');
-
-    //expose(date('m/d/y', $iEnd));
-    //stop();
-
 
     foreach($aEventData as $aEvent)
     {
       switch($aEvent['interval_id'])
       {
         case TYPE_INTERVAL_ONE_TIME:
-          //expose($aEvent);
           $sDate = $aEvent['interval_desc'];
           $iTs = Util::strtotime($sDate);
           if($iTs >= $iStart && $iTs <= $iEnd)
             $aCalcData[TYPE_INTERVAL_ONE_TIME][$iTs] = '<div class="clickable underlined" onclick="view_event('.$aEvent['id'].')">'.$aEvent['title'].'</div>';
           break;
         case TYPE_INTERVAL_WEEKLY:
-          //expose($aEvent);
-          // @TODO this depends on a barmend.com function
           $iTs = Util::strtotime(self::get_weekday_from_id($aEvent['interval_desc']));
           $sOrigDate = date(DATE_FORMAT_SHORT2, $iTs);
 
           if($iTs >= $iStart && $iTs <= $iEnd)
             $aCalcData[TYPE_INTERVAL_WEEKLY][$iTs] = '<div class="clickable underlined" onclick="view_event('.$aEvent['id'].')">'.$aEvent['title'].'</div>';
 
-          //$sOrigDate = $sDate;
           for($iWalk = 1; $iWalk <= 5; $iWalk++)
           {
             $iTs = Util::strtotime($sOrigDate.'+ '.(7 * $iWalk).' days');
-            //$sDate = date(DATE_FORMAT_SHORT2, $iTs);
-
-            //expose(date('n/j/y', $iTs));
-            //expose($aEvent['title']);
 
             if($iTs >= $iStart && $iTs <= $iEnd)
               $aCalcData[TYPE_INTERVAL_WEEKLY][$iTs] = '<div class="clickable underlined" onclick="view_event('.$aEvent['id'].')">'.$aEvent['title'].'</div>';
           }
           break;
         case TYPE_INTERVAL_MONTHLY:
-          //expose($aEvent);
           for($iWalk = 0; $iWalk <= 1; $iWalk++)
           {
             if($iWalk == 0)
@@ -458,11 +295,8 @@ class Calendar
               }
               else
                 $iMonth++;
-              //$iTs = Util::strtotime($aEvent['interval_desc'].' day next month');
             }
-            //expose($iMonth);
             $sDate = $iMonth.'/'.$aEvent['interval_desc'].'/'.$iYear;
-            //expose($sDate);
             $iTs = Util::strtotime($sDate);
             if($iTs >= $iStart && $iTs <= $iEnd)
               $aCalcData[TYPE_INTERVAL_MONTHLY][$iTs] = '<div class="clickable underlined" onclick="view_event('.$aEvent['id'].')">'.$aEvent['title'].'</div>';
@@ -470,7 +304,6 @@ class Calendar
           }
           break;
         case TYPE_INTERVAL_YEARLY:
-          //expose($aEvent);
           $sDate = $aEvent['interval_desc'].date('/Y');
           $iTs = Util::strtotime($sDate);
           if($iTs >= $iStart && $iTs <= $iEnd)
@@ -481,8 +314,7 @@ class Calendar
 
     // combine $aCalcData into 1 array - having more rare occurrences override
     $aTmp = array();
-
-
+    
     if(isset($aCalcData[TYPE_INTERVAL_WEEKLY]))
     {
       foreach($aCalcData[TYPE_INTERVAL_WEEKLY] as $iTs => $sDesc)
@@ -507,12 +339,7 @@ class Calendar
         $aTmp[$iTs] = $sDesc;
     }
 
-
-    //expose($aTmp);
-
     ksort($aTmp);
-
-    //expose($aTmp);
 
     $this->aUpcomingEvents = $aTmp;
   }

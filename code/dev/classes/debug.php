e134a409134201ebb8f9987bf18ed24634089a26
Keith Ritt <contact@keithritt.net>
2019-11-06 23:53:02 -0500

    Update debug.php

--- code/dev/classes/debug.php
+++ code/dev/classes/debug.php
@@ -1,14 +1,6 @@
 <?
-//neatly formats a var_dump
-
-//note php_sapi_name(): 'cgi-fcgi' is for web and cron
-
-
-
 class Debug
 {
-
-
   public static function init()
   {
     if(CODE_ENV == 'dev')
@@ -16,20 +8,12 @@ class Debug
       Request::$bDebugMode = true;
       error_reporting(E_ALL);
     }
-    //else
-    //  self::$bDebugMode = false;
-
   }
 
   public static function expose($var)
   {
-    //print "\n expose()";
   	if(Request::is_ajax())
     		return;
-    //print 'expose()';
-    //var_dump(ENVIRONMENT);
-    //var_dump(ENV);
-
     if(defined('LOG_FILE_MODE'))
       $sMode = 'cli'; // bit of a hack for scheduler
     else
@@ -45,7 +29,6 @@ class Debug
   			return; // no need for html in cli
         break;
       default:
-        //var_dump($_SESSION);
         if(!Request::$bDebugMode)
           return;
 
@@ -74,11 +57,8 @@ class Debug
 
     }
   }
-
-
   public static function pr($sText)
   {
-    //print "\npr()";
     if(Request::is_ajax())
         return;
 
@@ -98,15 +78,12 @@ class Debug
           return;
         $aTraces = debug_backtrace();
 
-
         foreach($aTraces as $aTrace)
         {
           if(isset($aTrace['class'], $aTrace['function']) && $aTrace['class'] == 'Debug' && $aTrace['function'] == 'pr')
             continue;
           break;
         }
-        //die();
-        //$aDebug = $aDebug[0];
 
         print '<br class="pr"><span style="font-family:verdana; background: white; color: black; font-family: verdana; font-size: 12px;">'.$sText.'</span>';
         $sId = 'pr_'.uniqid();
@@ -118,58 +95,30 @@ class Debug
 
   public static function line()
   {
-    //print "\nDebug::line()";
-
-    //print "\nLOG_FILE_MODE = ";
-
-    //var_dump(LOG_FILE_MODE);
-
-  	//print php_sapi_name();
-
-    //var_dump(defined('LOG_FILE_MODE'));
-
-   // die();
-
     if(defined('LOG_FILE_MODE') && LOG_FILE_MODE)
       $sMode = 'cli'; // but of a hack for scheduler
     else
       $sMode = php_sapi_name();
 
-    //print "\n hit line ".__line__;
-
-    //var_dump($sMode);
-
-    //die();
     switch($sMode)
     {
       case 'cli':
-          //$aDebug = debug_backtrace();
-          //$aDebug = $aDebug[0];
-          // @TODO - add microtime
           $aTraces = debug_backtrace();
           foreach($aTraces as $aTrace)
           {
-            //var_dump($aTrace);
-            //print "<br>hit debug.php line: ".__line__;
             if(isset($aTrace['file']) && strpos('_'.$aTrace['file'], 'debug.php'))
               continue;
             break;
           }
 
           print date('n/j/y H:i:s ').'hit line '.$aTrace['line'].' file: '.$aTrace['file']."\n";
-          //die();
   				return; // no need for html cli
         break;
       default:
-        //print "<br>hit debug.php line: ".__line__;
         if((!Request::$bDebugMode) && false) //@TODO - remove hack
         {
-          //print "<br>hit debug.php line: ".__line__;
           return;
         }
-        //else
-         // print "<br>hit debug.php line: ".__line__;
-
           $aTraces = debug_backtrace();
           foreach($aTraces as $aTrace)
           {
@@ -178,7 +127,6 @@ class Debug
               continue;
             break;
           }
-          //print "<br>hit debug.php line: ".__line__;
           print '<div style="color: black; background: white; font-family: verdana; font-size: 14px;">';
           print '<br>hit line '.$aTrace['line'];
           $sId = 'line_'.uniqid();
@@ -191,22 +139,14 @@ class Debug
 
   public static function expose_backtrace()
   {
-    //print "\n expose_backtrace()";
     if(!Request::$bDebugMode)
       return;
     $aTmp = array();
     $aTraces = debug_backtrace();
     foreach($aTraces as $aTrace)
     {
-  		//var_dump($aTrace);
   		if(isset($aTrace['file']))
       	$sTmp = 'File: '.$aTrace['file'].' Line: '.$aTrace['line'].' ';
-  		//else
-  	//	{
-  	//		line();
-    //			expose($aTrace);
-    //			die();//
-    //		}
       if(isset($aTrace['function']))
       {
         if(isset($aTrace['class']))
@@ -224,9 +164,7 @@ class Debug
       }
       $aTmp[] = $sTmp;
     }
-    //unset($aDebug[0]);
     self::expose($aTmp);
-    //die();
   }
 
   public static function check_mem($sText = null)
@@ -241,7 +179,6 @@ class Debug
     switch($sMode)
     {
       case 'cli':
-      //case 'cgi-fcgi':
         print $sText."\n";
         return; // no need for html in cli
         break;
@@ -261,7 +198,6 @@ class Debug
 
   public static function check_time($sText = null)
   {
-    //expose(FSTART);
     if(!defined('FSTART'))
       define('FSTART', microtime(true));
     $sText = 'Total Time: '.number_format(microtime(true) - FSTART, 4);
@@ -277,7 +213,6 @@ class Debug
     switch($sMode)
     {
       case 'cli':
-  		//case 'cgi-fcgi':
         print $sText."\n";
   			return; // no need for html in cli
         break;
@@ -322,7 +257,6 @@ class Debug
 function expose($sVar){Debug::expose($sVar);}
 function pr($sText){Debug::pr($sText);}
 function line(){
-  //print "<br>line()";
   Debug::line();}
 function expose_backtrace(){Debug::expose_backtrace();}
 function check_mem($sText = null){Debug::check_mem($sText);}
@@ -335,28 +269,15 @@ set_exception_handler('my_exception_handler');
 
 function my_error_handler($iErrNo, $sErrStr, $sErrFile, $iErrLine, $aErrContext)
 {
-
-  //expose(error_reporting());
   // todo - see if this is slowing down the site
   if(error_reporting() == 0 && $iErrNo == E_NOTICE)  // code was preceded with an @
     return;
 
-  //pr('my_error_handler('.$iErrNo.', '.$sErrStr.', '.$sErrFile.', '.$iErrLine.')');
-
-  //print '<br>my_error_handler()';
   // make sure the log class is loaded
-  //line();
   require_once(CODE_PATH.'/classes/db.php');
-  //line();
   require_once(CODE_PATH.'/classes/auth.php');
-  //line();
   require_once(CODE_PATH.'/classes/model.php');
-  //line();
   require_once(CODE_PATH.'/classes/log.php');
-  //line();
-  //require_once(CLASS_PATH.'libraries/universal_definitions.php');
-  //require_once(CLASS_PATH.'libraries/universal_functions.php');
-
   switch($iErrNo)
   {
     case E_WARNING: //2
@@ -377,15 +298,6 @@ function my_error_handler($iErrNo, $sErrStr, $sErrFile, $iErrLine, $aErrContext)
       $sErrorType = 'Unknown';
   }
 
-  //line();
-
-
-  //expose($iErrNo);
-  //expose($sErrStr);
-  //expose($sErrFile);
-  //expose($iErrLine);
-  //expose($aErrContext);
-
     $aParams = array(
       'sMsg' => $sErrStr,
       'sFile' => str_replace(CODE_PATH, '', $sErrFile),
@@ -395,14 +307,7 @@ function my_error_handler($iErrNo, $sErrStr, $sErrFile, $iErrLine, $aErrContext)
       'sCat' => $iErrNo,
       );
 
-    //line();
-
     Log::error($aParams);
-    //line();
-
-  //print '<br>';
-
-
 
 }
 
@@ -415,8 +320,3 @@ function my_exception_handler($oE)
   if(Request::$bDebugMode)
     stop();
 }
-
-//asdf();
-
-//$asdf = $asdf[4];
-

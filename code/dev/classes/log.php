1161429334653834c09fbfb63b829c7411a29e2e
Keith Ritt <contact@keithritt.net>
2019-11-06 23:58:10 -0500

    Update log.php

--- code/dev/classes/log.php
+++ code/dev/classes/log.php
@@ -19,44 +19,26 @@ class Log
 
   public static $iDomainId = 1; // @TODO - hardcoding DOMAIN_OFFICIALLOOP_COM;
   public static $iPriorityId = self::PRIORITY_MEDIUM;
-  //public static $oDb = null;
   public static $bPrint = false;
   private static $bPaused = false;
 
   public static function write($aParams)
   {
-    //pr('Log::write()');
-    //expose($aParams);
     // ingore if the log is paused
     if(self::$bPaused)
     {
-     // line();
       return;
     }
-    //else
-    //  line();
 
-    //line();
-
-    //self::init();
-    //line();
     if(is_array($aParams))
       extract($aParams);
     else
       $sMsg = $aParams;
 
-    //line();
-
     if(static::$bPrint)
       pr($sMsg);
 
-    //ine();
-
-		//line();
-
     $iPriorityId = @Util::coalesce($iPriorityId, static::$iPriorityId);
-
-    //line();
 
     if(!isset($sExpireTs))
     {
@@ -81,16 +63,11 @@ class Log
       }
     }
 
-    //line();
-
     $iTypeId = @Util::coalesce($iTypeId, self::TYPE_MSG);
     $iDomainId = @Util::coalesce($iDomainId, static::$iDomainId);
 
-    //line();
-
     if(!isset($sFile, $iLine))
     {
-      //line();
       $aDebug = debug_backtrace();
       foreach($aDebug as $aStep)
       {
@@ -102,19 +79,7 @@ class Log
 
           break;
       }
-      //expose($aDebug);
-      //stop();
-
     }
-			//line();
-    //expose($sCat);
-    //expose($sSubCat);
-
-    //line();
-
-    //expose(Db::$oMaster);
-
-    //stop();
 
     $oLog = Model::init('master', 'logs', Db::$oMaster);
 		//line();
@@ -133,58 +98,33 @@ class Log
     if(isset($sExtra))
       $oLog->extra = $sExtra;
 
-    //expose(PAGE_VIEW_ID);
-    //stop();
     if(defined('PAGE_VIEW_ID'))
       $oLog->page_view_id = PAGE_VIEW_ID;
     if(defined('SCHEDULER_ID'))
       $oLog->scheduler_id = SCHEDULER_ID;
 
-
-    //pr('save log');
     $oLog->save();
-		//line();
-
-    //expose($oLog->aData);
-    //die();
-    //expose($sExpireTs);
   }
 
   public static function _error($sMsg, $iPriorityId = self::PRIORITY_CRITICAL, $iTypeId = self::TYPE_PHP_ERROR, $sExtra = null, $sFile = null, $iLine = null)
   {
-    //pr('Log::_error()');
     return self::error(array('sMsg' => $sMsg, '', 'iPriorityId' => $iPriorityId, 'iTypeId' => $iTypeId, 'sExtra' => $sExtra, 'sFile' => $sFile, 'iLine' => $iLine));
   }
 
   public static function error($aParams)
   {
-
-    //expose(self::$bPaused);
-    // ignore if the log is paused
-    //line();
     if(self::$bPaused)
       return;
 
-    //line();
-
-
-
-
-		//expose(php_sapi_name());
-		//expose($aParams);
 		if(!is_array($aParams))
     {
-      //line();
 				$sMsg = $aParams;
       $aParams = array();
       $aParams['sMsg'] = $sMsg;
     }
-    //pr('Log->error('.$aParams['sMsg'].')');
 		//line();
     $aParams['iTypeId'] = @Util::coalesce($aParams['iTypeId'], self::TYPE_PHP_ERROR);
-    //line();
     self::write($aParams);
-		//line();
 		if(Request::$bDebugMode) //&& php_sapi_name() == 'cgi-fcgi'
 		{
       if(!isset($aParams['sFile'], $aParams['iLine']))
@@ -202,28 +142,24 @@ class Log
 
         }
       }
-			//line();
-      //expose($aParams);
+
 			@pr('ERROR: '.$aParams['sCat'].': '.$aParams['sMsg'].' '.$aParams['sFile'].' '.$aParams['iLine']);
-      //line();
+
 			expose_backtrace();
 
       self::$bPaused = true; // avoid infinite loops
-      //line();
 			stop();
 		}
   }
 
 	public static function deprecated_code($sMsg = null)
 	{
-    //pr('Log::deprecated_code('.$sMsg.')');
     if(is_null($sMsg))
       Log::deprecated_code('Log::deprecated_code() without $sMsg');
 		$aParams = array();
 		$aTrace = debug_backtrace();
 		$aTrace = $aTrace[1];
-		//expose($aTrace);
-    //stop();
+
 		$aParams['sMsg'] = $sMsg;//. '<br/> @ File: '.$aTrace['file'].' line: '.$aTrace['line'];
 		$aParams['sCat'] = 'Deprecated Code';
     $aParams['sFile'] = $aTrace['file'];
@@ -231,28 +167,13 @@ class Log
 		self::error($aParams);
 	}
 
-  //private static function init()
-  //{
-    //pr('Log::init()');
-    //if(is_null(static::$oDb))
-    //{
-    //  line();
-     // self::$oDb = new Db();
-    //  line();
-   // }
-   // else
-   //   line();
-  //}
-
   public static function pause()
   {
-    //pr('Log::pause()');
     self::$bPaused = true;
   }
 
   public static function resume()
   {
-    //pr('Log::resume()');
     self::$bPaused = false;
   }
 }
